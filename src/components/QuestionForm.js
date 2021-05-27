d4bb83a67a8077f48c760d16c48f0f1bde6b243e
WADJI T. Thierry <wadjith@yahoo.fr>
2021-05-27 08:29:37 +0100

    Update QuestionForm.js
    Pour utiliser les formulaires contrôlés.

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,17 +1,23 @@
+import {useState} from 'react';
 
 function QuestionForm() {
+    const [inputValue, setInputValue] = useState("Posez votre question ici");
     return (
-    <form onSubmit={handleSubmit}>
-        <input type='text' name='my_input' defaultValue='Tapez votre texte' />
-        <button type='submit'>Entrer</button>
-    </form>
+        <div>
+            <textarea
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+        </div>
+    
     );
 }
 
-
+/*
 function handleSubmit(e) {
     e.preventDefault()
     alert(e.target['my_input'].value)
-}
+}*/
 
 export default QuestionForm;
